15323d0401fdfb460b1625c922f7ff70c28ee47a
Davor Runje <zlikowski@gmail.com>
2025-04-09 16:27:59 +0200

    CI fixes

---
+++ tests/test_dummy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+def test_dummy() -> None:
+    """Dummy test to ensure pytest is working."""
+    assert True
